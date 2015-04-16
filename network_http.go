f034254a25ac6f822091e2a5eb3c062a59bbf659
Norwack <linuxguy@1337.no>
2015-04-16 08:59:40 +0200

    this is turning into a mess, so a mass commit..

--- network_http.go
+++ network_http.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"github.com/samuel/go-socks/socks"
-)
-
 func ScanHTTP(onion string) bool {
-	proxy := &socks.Proxy{Addr: "127.0.0.1:9050"}
-	conn, err := proxy.Dial("tcp", onion + ":80")
+	conn, err := Connect(onion + ":80")
 	if err != nil {
 		return false
 	}
