c2cb5bdd02bd6f4d9e645bc717802e57c79af803
Norwack <linuxguy@1337.no>
2015-04-16 08:52:17 +0200

    forgot i wasn't working directly with net..

--- network.go
+++ network.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"net"
+	"github.com/samuel/go-socks/socks"
 )
 
 type Network struct {
-	Conn  *net.Conn
+	Conn  *socks.Proxy
 }
 
 func Connect(host string) (*Network, error) {
-	conn, err := net.Dial("tcp", host)
+	proxy := &socks.Proxy{Addr: "127.0.0.1:9050"}
+	conn, err := proxy.Dial("tcp", host)
 	if err != nil {
 		return &Network{}, err
 	}
